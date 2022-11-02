a4e6ede638d725c24064dda1c5fc69231264d836
Pratik Dhungel <pratikdhungel7.0@gmail.com>
2022-11-02 14:11:29 +0545

    Learning Vue JS: Initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button label="Add Text" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  name: 'AppHeader',
+  props: ['title'],
+  components: {
+    Button,
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<template>
+  <button :style="{ background: color }" class="btn">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: ['label', 'color'],
+}
+</script>
